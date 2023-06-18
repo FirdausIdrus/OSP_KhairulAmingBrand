690715aac1da36952e89bf3cb4b133b1c9cd275f
Daus1256 <u2001256@siswa.um.edu.my>
2023-06-18 23:44:23 +0800

    footer already in separate file

---
+++ footer.php
@@ -0,0 +1,102 @@
+<!--footer-->
+<footer class="site-footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-12 me-auto mb-5 mb-lg-0">
+
+                        <a class="navbar-brand d-flex align-items-center" href="index.html">
+                            <span class="navbar-brand-text">
+                                Sambal <br>
+                                Nyet <br>
+                                Berapi
+                            </span>
+                        </a>
+                        <br>
+
+                        <div class="row">
+                            <h5 class="site-footer-title mb-4">Sambal Nyet HQ</h5>
+
+                            <p class="d-flex me-lg-3">
+                                <span>Jalan Melati Indah, Taman Seri Delima, 123, 
+                                    51200 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur</span>
+                            </p>
+                            <h5 class="site-footer-title mb-4">Email</h5>
+
+                            <p class="d-flex me-lg-3">
+                                <span> sambalnyet@gmail.com</span>
+                            </p>
+                            <h5 class="site-footer-title mb-4">Phone</h5>
+
+                            <p class="d-flex me-lg-3">
+                                <span>+603 - 78721999</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-12">
+                        <h5 class="site-footer-title mb-4">Company Profile</h5>
+
+                        <p class="d-flex me-lg-3">
+                            <span>About Us</span>
+                        </p>
+
+                        <p class="d-flex me-lg-3">
+                            <span>Our Journey</span>
+                        </p>
+
+                        <p class="d-flex me-lg-3">
+                            <span>Our Product</span>
+                        </p>
+
+                        <p class="d-flex me-lg-3">
+                            <span>Community</span>
+                        </p>
+
+                        <p class="d-flex me-lg-3">
+                            <span>Join Us</span>
+                        </p>
+                    </div>
+
+                        <div class="col-lg-2 col-12 ms-auto">
+                            <h5 class="site-footer-title mb-4">Help & Support</h5>
+
+                            <p class="d-flex me-lg-3">
+                                <span>Privacy Policy</span>
+                            </p>
+
+                            <p class="d-flex me-lg-3">
+                                <span>Terms & Condition</span>
+                            </p>
+
+                            <p class="d-flex me-lg-3">
+                                <span>Contact Us</span>
+                            </p>
+
+                            <p class="d-flex me-lg-3">
+                                <span>F.A.Q</span>
+                            </p>
+                            <br>
+                            <br>
+
+                            <ul class="social-icon mt-lg-5 mt-3 mb-4">
+                                <li class="social-icon-item">
+                                    <a href="#" class="social-icon-link bi-instagram"></a>
+                                </li>
+
+                                <li class="social-icon-item">
+                                    <a href="#" class="social-icon-link bi-twitter"></a>
+                                </li>
+
+                                <li class="social-icon-item">
+                                    <a href="#" class="social-icon-link bi-tiktok"></a>
+                                </li>
+                            </ul>
+                            <p class="copyright-text">Design: <a rel="nofollow" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+                            
+                        </div>
+
+                </div>
+            </div>
+        </footer>
+        <!--End of footer-->
